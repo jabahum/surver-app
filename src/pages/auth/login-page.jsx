e908c8b336bf26414d2f781fcc547528d8e89fd2
jabahum <jabajnan8@gmail.com>
2023-05-21 13:56:03 +0300

    ft : add buttons

--- src/pages/auth/login-page.jsx
+++ src/pages/auth/login-page.jsx
@@ -1,9 +1,49 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 export default function LoginPage() {
-  
-    return (
-      <div id="login-page">
-        <h1>Login!</h1>
-    
-      </div>
-    );
-  }
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <Container>
+        <form onSubmit={onSubmit}>
+          <h1>Login </h1>
+
+          <div>
+            <TextField
+              id="email"
+              label="Email"
+              variant="outlined"
+              onChange={this.setEmail}
+              value={this.state.email}
+            />
+          </div>
+          <br />
+          <div>
+            <TextField
+              id="password"
+              label="Password"
+              variant="outlined"
+              onChange={this.setPassword}
+              value={this.state.password}
+            />
+          </div>
+          <br />
+          <div>
+            <Button id="btnLogin" variant="contained">
+              Login
+            </Button>
+          </div>
+        </form>
+      </Container>
+    </>
+  );
+}
--- src/pages/auth/signup-page.jsx
+++ src/pages/auth/signup-page.jsx
@@ -1,9 +1,50 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+
 export default function SignUpPage() {
-  
-    return (
-      <div id="signup-page">
-        <h1>SignUp!</h1>
-    
-      </div>
-    );
-  }
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <Container>
+        <form onSubmit={onSubmit}>
+          <h1>Login </h1>
+
+          <div>
+            <TextField
+              id="email"
+              label="Email"
+              variant="outlined"
+              onChange={this.setEmail}
+              value={this.state.email}
+            />
+          </div>
+          <br />
+          <div>
+            <TextField
+              id="password"
+              label="Password"
+              variant="outlined"
+              onChange={this.setPassword}
+              value={this.state.password}
+            />
+          </div>
+          <br />
+          <div>
+            <Button id="btnLogin" variant="contained">
+              Login
+            </Button>
+          </div>
+        </form>
+      </Container>
+    </>
+  );
+}
